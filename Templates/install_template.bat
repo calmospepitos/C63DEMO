5a93dbad2e44f8640e15ec2f17d6cb489727eb1e
Alexia Levesque <88449215+calmospepitos@users.noreply.github.com>
2025-02-26 21:10:14 -0500

    Organisation des dossiers/fichiers de la solution et exercice logger, exercice pacman et exercice preprocesseur fait.

--- Templates/install_template.bat
+++ Templates/install_template.bat
@@ -1,2 +1,7 @@
-copy .\* "C:\Users\%USERNAME%\Documents\Visual Studio 2022\My Exported Templates"
+if not exist "C:\Users\%USERNAME%\Documents\Visual Studio 2022\Templates\ProjectTemplates\My Exported Templates" (
+    mkdir "C:\Users\%USERNAME%\Documents\Visual Studio 2022\Templates\ProjectTemplates\My Exported Templates"
+)
+
+copy .\* "C:\Users\%USERNAME%\Documents\Visual Studio 2022\Templates\ProjectTemplates\My Exported Templates"
+
 pause
